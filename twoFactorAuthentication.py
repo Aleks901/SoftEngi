7fd9011e7fbe7dc64d1d655ebb98614d86d4091c
Aleks901 <aleks-j29@hotmail.com>
2024-10-21 15:15:44 +0200

    Lagt til magisk funksjon
    Den magiske funksjonen som setter sammen hvor vidt passordet ditt var korrekt og hvor vidt <vilkårelig_authentication_app>.get returnerer sann verdi er implementert!

--- twoFactorAuthentication.py
+++ twoFactorAuthentication.py
@@ -3,3 +3,9 @@ class twoFactAuth():
   
   def __init__(self) -> None:
     pass
+  
+  def magic(password: bool, auth: bool):
+    if password and auth == True:
+      print("Woah! It worked!")
+    else:
+      print("Something's wrong!")
